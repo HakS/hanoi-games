c0aad9186ee7b7f2f8c373182f4ebfd1456eba9d
Juan Barba <juan.barba.o@gmail.com>
2015-12-14 20:42:37 -0500

    removed unneeded code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,7 +10,6 @@
  */
 angular
     .module('haksGamesApp', ['ngRoute', 'djds4rce.angular-socialshare'])
-    //.config(function ($routeProvider, $locationProvider) {
     .config(function ($routeProvider) {
         $routeProvider
             .when('/', {
@@ -23,54 +22,15 @@ angular
                 controller: 'GamePageCtrl',
                 controllerAs: 'gamePage',
             })
-            //.when('/game/hanoi', {
-            //    templateUrl: 'views/games/hanoi.html',
-            //    controller: 'HanoiCtrl',
-            //    controllerAs: 'hanoi',
-            //    game: 'Hanoi Towers'
-            //})
-            //.when('/game/maze', {
-            //    templateUrl: 'views/games/maze.html',
-            //    controller: 'MazeCtrl',
-            //    controllerAs: 'maze',
-            //    game: 'The Maze'
-            //})
-            //.when('/game/tictactoe', {
-            //    templateUrl: 'views/games/tictactoe.html',
-            //    controller: 'TictactoeCtrl',
-            //    controllerAs: 'tictactoe',
-            //    game: 'Tic-Tac-Toe'
-            //})
-            //.when('/game/tetris', {
-            //    templateUrl: 'views/games/tetris.html',
-            //    controller: 'TetrisCtrl',
-            //    controllerAs: 'tetris',
-            //    game: 'Tetris'
-            //})
             .otherwise({
                 redirectTo: '/'
             });
-            //if (window.history && window.history.pushState) {
-            //    $locationProvider.html5Mode(true).hashPrefix('!');
-            //}
     })
     .run(function ($rootScope) {
         $rootScope.games = [
             {
                 id: 'hanoi',
                 title: 'Hanoi Towers'
-            },
-            {
-                id: 'maze',
-                title: 'The Maze!'
-            },
-            {
-                id: 'tetris',
-                title: 'Tetris'
-            },
-            {
-                id: 'tictactoe',
-                title: 'Tic-Tac-Toe'
             }
         ];
     });
